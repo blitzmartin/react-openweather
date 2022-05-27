17c63e84f0c97ea47760fdb23b5e76f2aa57f1e1
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-27 16:37:11 +0200

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import WbSunnyIcon from '@mui/icons-material/WbSunny';
 export default function Header () {
     return (
         <div className="header">
-            <h1><WbSunnyIcon sx={{ fontSize: "3rem" }} /> Weather Forecast</h1>
+            <h1><span className="logo"><WbSunnyIcon  sx={{ fontSize: "3rem" }} /> </span>Weather Forecast</h1>
         </div>
     )
 }
